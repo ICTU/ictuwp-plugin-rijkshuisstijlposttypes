993c0edf42db00c29dcacb0085897a80a3a87a09
Paul van Buuren <paul@wbvb.nl>
2020-05-13 19:10:55 +0200

    administratie

--- distribute.sh
+++ distribute.sh
@@ -1,72 +1,65 @@
 
 
-# sh '/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-Custom-Post-Types-and-Taxonomies/distribute.sh' &>/dev/null
+
+# sh '/Users/paul/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-Custom-Post-Types-and-Taxonomies/distribute.sh' &>/dev/null
 
 echo '----------------------------------------------------------------';
 echo 'Distribute DO post type plugin';
 
+
+
+
 # clear the log file
-> '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/debug.log'
-> '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/gc_live_import/wp-content/debug.log'
+> '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/debug.log'
 
 # copy to temp dir
-rsync -r -a --delete '/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-Custom-Post-Types-and-Taxonomies/' '/shared-paul-files/Webs/temp/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/git-repos/Digitale-Overheid---WordPress-Custom-Post-Types-and-Taxonomies/' '/Users/paul/shared-paul-files/Webs/temp/'
 
 # clean up temp dir
-rm -rf '/shared-paul-files/Webs/temp/.git/'
-rm '/shared-paul-files/Webs/temp/.gitignore'
-rm '/shared-paul-files/Webs/temp/config.codekit3'
-rm '/shared-paul-files/Webs/temp/distribute.sh'
-rm '/shared-paul-files/Webs/temp/README.md'
-rm '/shared-paul-files/Webs/temp/LICENSE'
+rm -rf '/Users/paul/shared-paul-files/Webs/temp/.git/'
+rm '/Users/paul/shared-paul-files/Webs/temp/.gitignore'
+rm '/Users/paul/shared-paul-files/Webs/temp/.config.codekit3'
+rm '/Users/paul/shared-paul-files/Webs/temp/config.codekit3'
+rm '/Users/paul/shared-paul-files/Webs/temp/distribute.sh'
+rm '/Users/paul/shared-paul-files/Webs/temp/README.md'
+rm '/Users/paul/shared-paul-files/Webs/temp/LICENSE'
+
+
+cd '/Users/paul/shared-paul-files/Webs/temp/';
+find . -name '*.map' -type f -delete;
+find . -name '.DS_Store' -type f -delete
 
 
 # --------------------------------------------------------------------------------------------------------------------------------
 # Vertalingen --------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------------------
 # remove the .pot
-rm '/shared-paul-files/Webs/temp/languages/rhswp-posttypes.pot'
+rm '/Users/paul/shared-paul-files/Webs/temp/languages/ictuwp-plugin-rijkshuisstijlposttypes.pot'
 
 # copy files to /wp-content/languages/themes
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/languages/plugins/'
 
-# languages erics server
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/live-dutchlogic/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/languages/plugins/'
+
 
 # languages Sentia accept
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/languages/plugins/'
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/languages/plugins/'
 
 # languages Sentia live
-rsync -ah '/shared-paul-files/Webs/temp/languages/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/languages/plugins/'
-
-
-
-
-
-cd '/shared-paul-files/Webs/temp/'
-find . -name ‘*.DS_Store’ -type f -delete
+rsync -ah '/Users/paul/shared-paul-files/Webs/temp/languages/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/languages/plugins/'
+		
 
 
 # copy from temp dir to dev-env
-rsync -r -a --delete '/shared-paul-files/Webs/temp/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/rhswp-register_taxonomies/' 
-
-# remove temp dir
-rm -rf '/shared-paul-files/Webs/temp/'
-
-
-
-# Naar GC import
-rsync -r -a  --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/rhswp-register_taxonomies/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/gc_live_import/wp-content/plugins/rhswp-register_taxonomies/'
-
-# Naar Eriks server
-rsync -r -a  --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/rhswp-register_taxonomies/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/live-dutchlogic/wp-content/plugins/rhswp-register_taxonomies/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/ictuwp-plugin-rijkshuisstijlposttypes/' 
 
 # en een kopietje naar Sentia accept
-rsync -r -a --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/rhswp-register_taxonomies/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/plugins/rhswp-register_taxonomies/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/accept/www/wp-content/plugins/ictuwp-plugin-rijkshuisstijlposttypes/'
 
 # en een kopietje naar Sentia live
-rsync -r -a --delete '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/development/wp-content/plugins/rhswp-register_taxonomies/' '/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/plugins/rhswp-register_taxonomies/'
+rsync -r -a --delete '/Users/paul/shared-paul-files/Webs/temp/' '/Users/paul/shared-paul-files/Webs/ICTU/Gebruiker Centraal/sentia/live/www/wp-content/plugins/ictuwp-plugin-rijkshuisstijlposttypes/'
 
+# remove temp dir
+rm -rf '/Users/paul/shared-paul-files/Webs/temp/'
 
 echo 'Ready';
 echo '----------------------------------------------------------------';
